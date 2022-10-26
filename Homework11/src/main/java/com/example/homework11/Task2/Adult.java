c200877e7b041ff38ef68de39e934f084b689657
Idorio <insaider9218@gmail.com>
2022-10-26 19:02:25 +0300

    HomeWork 11 part 1 v.2

--- Homework11/src/main/java/com/example/homework11/Task2/Adult.java
+++ Homework11/src/main/java/com/example/homework11/Task2/Adult.java
@@ -1,10 +1,34 @@
 package com.example.homework11.Task2;
 
 import java.util.ArrayList;
+;
+import java.util.function.Predicate;
+import java.util.stream.Stream;
 
 public class Adult {
     public static void main(String[] args) {
-        ArrayList<Adult> adults = new ArrayList<>();
-        adults.add(new Adult())
+
+        ArrayList<Values> values = new ArrayList<>();
+        values.add(new Values(23, "Pasha"));
+        values.add(new Values(31, "Jenia"));
+        values.add(new Values(27, "Andrei"));
+        values.add(new Values(45, "Victor"));
+
+
+
+        Stream<Values> oldestAdult = values.stream().skip(1)
+                .limit(2);  //  немогу разобрать как отфильтровать по возрасту
+
+        System.out.print("The oldest adult ");
     }
-}
+
+    }
+
+
+
+
+
+
+
+
+
--- Homework11/src/main/java/com/example/homework11/Task2/Values.java
+++ Homework11/src/main/java/com/example/homework11/Task2/Values.java
@@ -1,4 +1,21 @@
 package com.example.homework11.Task2;
 
 public class Values {
+    final String name;
+    final Integer age;
+
+
+    public Values( int age,String name){
+        this.name = name;
+        this.age = age;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    public Integer getAge() {
+        return age;
+    }
+
 }
